a49858cecadefa9fcd3f2bd37a23a16e8ffc6837
eyapeleg <eyal.peleg@intel.com>
2018-07-22 11:50:47 +0300

    add exports

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -e
 
 aws cloudformation update-stack \
     --stack-name cfn-demo-stack \
     --template-body file://template.yaml \
     --parameters ParameterKey=Env,ParameterValue=foo ParameterKey=FunctionName,ParameterValue=CfnDemo \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --profile $1
+
+aws cloudformation create-stack \
+    --stack-name cfn-demo-stack-2 \
+    --template-body file://template-2.yaml \
+    --parameters ParameterKey=Env,ParameterValue=foo ParameterKey=FunctionName,ParameterValue=CfnDemoTwo \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile $1
