ea197284f1d1bc0593d646d233f6ecc82a96859f
eyapeleg <eyal.peleg@intel.com>
2018-07-22 11:37:57 +0300

    add references

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Parameters
+References
 ----------
 
-In this section we will learn how to inject a parameter to the cfn stack.
+In this section we will get acquainted with two types of references:
+    1. Ref function
+    2. GetAtt function
+
+In addition we will examine a more complex variation of !Sub function
 
--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 aws cloudformation update-stack \
     --stack-name cfn-demo-stack \
     --template-body file://template.yaml \
-    --parameters ParameterKey=Env,ParameterValue=foo \
+    --parameters ParameterKey=Env,ParameterValue=foo ParameterKey=FunctionName,ParameterValue=CfnDemo \
     --capabilities CAPABILITY_NAMED_IAM \
     --profile $1
