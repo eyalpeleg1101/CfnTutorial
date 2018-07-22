1484b3e618841b5fff4d253f14cf7c68676caac2
eyapeleg <eyal.peleg@intel.com>
2018-07-22 16:00:13 +0300

    bug fix

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Mappings
 --------
 
 Mappings enable us to **map an input parameter to a set of constant values**.
-e.g. for account=preprod use this EC2 instance and for account=prod is a stronger instance.
+
+e.g. for account *preprod* use a certain EC2 instance and for account *prod* user a stronger instance.
 
 
--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,21 @@ set -e
 
 BUCKET_NAME=aah-general
 
+ENV=$1
+PROFILE=$2
+
 aws s3 cp \
     template-nested.yaml \
     "s3://${BUCKET_NAME}/" \
-    --profile $1
+    --profile ${PROFILE}
 
 aws cloudformation update-stack \
-    --stack-name cfn-demo-stack \
+    --stack-name ${ENV}-cfn-demo-stack \
     --template-body file://template.yaml \
     --parameters \
-      ParameterKey=Env,ParameterValue=foo \
+      ParameterKey=Env,ParameterValue=${ENV} \
       ParameterKey=FunctionName,ParameterValue=CfnDemo \
       ParameterKey=BucketUrl,ParameterValue="https://s3.amazonaws.com/${BUCKET_NAME}/template-nested.yaml" \
       ParameterKey=Account,ParameterValue=preprod \
     --capabilities CAPABILITY_NAMED_IAM \
-    --profile $1
+    --profile ${PROFILE}
