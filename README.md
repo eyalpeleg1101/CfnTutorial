9c46e24ff5d5657c94d70c66f7a4aef86ebd4697
eyapeleg <eyal.peleg@intel.com>
2018-07-22 15:20:31 +0300

    bug fix

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ References
 ----------
 
 In this section we will get acquainted with two types of references:
-    1. Ref function
-    2. GetAtt function
+    1. `!Ref` function
+    2. `!GetAtt` function
 
-In addition we will examine a more complex variation of !Sub function
+In addition we will examine a more **complex variation of `!Sub`** function
 
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+ENV=$1
+PROFILE=$2
+
 aws cloudformation update-stack \
-    --stack-name cfn-demo-stack \
+    --stack-name ${ENV}-cfn-demo-stack \
     --template-body file://template.yaml \
-    --parameters ParameterKey=Env,ParameterValue=foo ParameterKey=FunctionName,ParameterValue=CfnDemo \
+    --parameters \
+      ParameterKey=Env,ParameterValue=${ENV} \
+      ParameterKey=FunctionName,ParameterValue=CfnDemo \
     --capabilities CAPABILITY_NAMED_IAM \
-    --profile $1
+    --profile ${PROFILE}
