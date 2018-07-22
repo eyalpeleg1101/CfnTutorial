53ba92d5b1b0563b6eeff7c77ec2195f0728e3c9
eyapeleg <eyal.peleg@intel.com>
2018-07-22 11:10:28 +0300

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ aws cloudformation create-stack \
     --stack-name cfn-demo-stack \
     --template-body file://template.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
-    --profile health
+    --profile $1
